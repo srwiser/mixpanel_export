3bf4be320337f591232598dc7d25ec468bf27c8a
swapnil <swapnil@getvymo.com>
2017-05-11 23:46:21 +0530

    altered if condition

--- batch-convert.sh
+++ batch-convert.sh
@@ -2,8 +2,11 @@
 for filename in *.json; do
 	jsonfile=$filename
 	csvfile=`ls $filename | cut -d '.' -f 1`.csv
-	if [ ! -f $csvfile ]; then
-		echo "Creating $csvfile"
+	if [ -f $csvfile ]; then
+		echo "CSV already exist for $jsonfile"
+	else
+		echo "Creating csv for $jsonfile"
 		python jsontocsv.py $jsonfile $csvfile
 	fi
 done
+exit 0
